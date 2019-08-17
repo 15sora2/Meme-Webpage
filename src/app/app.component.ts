6b522952e9f380721581770a848dd2e12754c315
15sora2 <stgordon19@gmail.com>
2019-08-17 11:16:25 -0500

    Change root page image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'meme-app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'meme-app';
+  title: string = 'Meme Galore';
 }
