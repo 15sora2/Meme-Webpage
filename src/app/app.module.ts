b93d75475d1fa82eb2c88becdeb3b8b0a9644f02
15sora2 <stgordon19@gmail.com>
2019-08-18 09:08:24 -0500

    Began table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { memeListComponent } from './memes/meme-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    memeListComponent
   ],
   imports: [
     BrowserModule,
