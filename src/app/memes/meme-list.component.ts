450a46c75978799de4e6edb614a300d9aaf7bc79
15sora2 <stgordon19@gmail.com>
2019-08-18 09:32:44 -0500

    Created json, added data to table

--- src/app/memes/meme-list.component.ts
+++ src/app/memes/meme-list.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 
 export class memeListComponent{
     mainMemeList: string = 'List of Memes';
+    memes: any[] = [
+        {
+            "memeTitle": "Stage Fright Mob",
+            "memeKeyword": "MobPsycho",
+            "likes": 5,
+            "dislikes": 1,
+            "memeUrl": "assets/memes/mobSpeechless.jpg"
+        },
+        {
+            "memeTitle": "Hulk Banner", 
+            "memeKeyword": "Hulk",
+            "likes": 67,
+            "dislikes": 45,
+            "memeUrl":"assets/memes/hulkBanner.jpg"
+        },
+        {
+            "memeTitle": "Tired Spongebob",
+            "memeKeyword": "Spongebob",
+            "likes": 78,
+            "dislikes": 24,
+            "memeUrl":"assets/memes/tiredSpngeBob.jpg"
+        }
+    ]
 }
